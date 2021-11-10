b67264b0b84c529724452958f5438ed2a5d82490
Leandro Silva <lemesilemesi@gmail.comm>
2021-11-10 08:19:07 -0300

    10nov

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace PetShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            //cors
             app.UseCors(options =>
             options.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
